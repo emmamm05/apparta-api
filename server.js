73c1539928ae2144e19a57af230f0400acf7e260
kamadrigal <karlamv93@gmail.com>
2014-05-20 08:22:08 -0600

    Modificaciones API, y Avance CRUD API apartamento

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var Bear     = require('./app/models/bear');
-
 var Usuario    = require('./app/models/usuario');
 var Token    = require('./app/models/usuario');
 var Apartamento = require('./app/models/apartamento');
@@ -39,7 +37,7 @@ router.get('/', function(req, res) {
 });
 
 //TOKENS routes
-router.route('/tokens')
+/*router.route('/tokens')
 	// create a token (accessed at POST http://localhost:8080/api/tokens)
 	.post(function(req, res) {
 		
@@ -77,12 +75,12 @@ router.route('/cuenta')
 	.put(function(req, res) {
 
 		// use our bear model to find the bear we want
-		Bear.findById(req.params.cuenta_id, function(err, cuenta) {
+		Usuario.findById(req.params.usuario_id, function(err, usuario) {
 
 			if (err)
 				res.send(err);
 
-			bear.name = req.body.name; 	// update the bears info
+			usuario.name = req.body.name; 	// update the bears info
 
 			// save the bear
 			bear.save(function(err) {
@@ -203,33 +201,95 @@ router.route('/apartamento/search?user_id&user_token&app_token&search_params')
 				res.send(err);
 			res.json(bear);
 		});
-	});
+	});*/
 
-//Ver Información de apartamento
-router.route(' /apartamento/aparta_id?app_token')
-	.get(function(req, res) {
-		Bear.findById(req.params.user_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	});
+
+
 //Agregar nuevo apartamento
-router.route('/apartamento?app_token')
+router.route('/apartamentos') //?app_token
 	.post(function(req, res) {
 		
-		var tokens = new Token(); 		// create a new instance of the Token model
-		tokens.token = req.body.token;  // set the tokens token (comes from the request)
+		var apartamento = new Apartamento(); 		
+		apartamento.descripcion = req.body.descripcion;
+		apartamento.direccion_fisica = req.body.direccion_fisica;
+		apartamento.area = req.body.area;
+		apartamento.ubicacion_latitud = req.body.ubicacion_latitud;
+		apartamento.ubicacion_longitud = req.body.ubicacion_longitud;
+		apartamento.cercania_tec = req.body.cercania_tec;
+		apartamento.mensualidad= req.body.mensualidad;
+		apartamento.habitaciones= req.body.habitaciones;
+		apartamento.titulo= req.body.titulo;
+		apartamento.genero= req.body.genero;
+		apartamento.opcion_agua= req.body.opcion_agua;
+		apartamento.opcion_electricidad= req.body.opcion_electricidad;
+		apartamento.opcion_seguridad= req.body.opcion_seguridad;
+		apartamento.opcion_internet= req.body.opcion_internet; 
+		 
 
 		// save the token and check for errors
-		tokens.save(function(err) {
+		apartamento.save(function(err) {
 			if (err)
 				res.send(err);
 
 			res.json({ message: 'Token created!' });
 		});
 		
+	});
+
+//Ver Información de apartamento
+router.route(' /apartamentos/:aparta_id')//?app_token
+	.get(function(req, res) {
+		Apartamento.findById(req.params.aparta_id, function(err, apartamento) {
+			if (err)
+				res.send(err);
+			res.json(apartamento);
+		});
 	})
+//Modificar Información de apartamento
+	.put(function(req, res) {
+
+		// use our bear model to find the bear we want
+		Apartamento.findById(req.params.aparta_id, function(err, apartamento) {
+
+			if (err)
+				res.send(err);
+
+			apartamento.descripcion = req.body.descripcion;
+			apartamento.direccion_fisica = req.body.direccion_fisica;
+			apartamento.area = req.body.area;
+			apartamento.ubicacion_latitud = req.body.ubicacion_latitud;
+			apartamento.ubicacion_longitud = req.body.ubicacion_longitud;
+			apartamento.cercania_tec = req.body.cercania_tec;
+			apartamento.mensualidad= req.body.mensualidad;
+			apartamento.habitaciones= req.body.habitaciones;
+			apartamento.titulo= req.body.titulo;
+			apartamento.genero= req.body.genero;
+			apartamento.opcion_agua= req.body.opcion_agua;
+			apartamento.opcion_electricidad= req.body.opcion_electricidad;
+			apartamento.opcion_seguridad= req.body.opcion_seguridad;
+			apartamento.opcion_internet= req.body.opcion_internet; 
+			// save the apartment
+			apartamento.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Apartament updated!' });
+			});
+
+		});
+	})
+//Remover apartamento
+	.delete(function(req, res) {router.route('/apartamentos')
+		Apartamento.remove({
+			_id: req.params.aparta_id
+		}, function(err, token) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+/*
 //Cargar apartamentos
 	.get(function(req, res) {
 		Bear.findById(req.params.user_id, function(err, bear) {
@@ -279,19 +339,7 @@ router.route('/apartas/aparta_id/calificacion')
 		});
 		
 	});
-
-//Remover apartamento
-router.route('/apartas/aparta_id')
-	.delete(function(req, res) {router.route('/tokens')
-		Token.remove({
-			token_id: req.params.user_id
-		}, function(err, token) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
+*/
 
 
 /*
