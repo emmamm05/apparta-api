5f14724f9270b58402d303376678b3aa632d4cca
emmamm05 <emmanuelmora05@gmail.com>
2014-06-15 19:40:46 -0600

    Fix

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ router.use(function(req, res, next) {
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('*', function(req, res) {
+router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
